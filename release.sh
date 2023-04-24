4ae114650fa4f9f974d90dcb03b35d833b907005
ferrreo <harderthanfire@gmail.com>
2023-04-24 17:18:59 +0100

    Update to lunar

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ dpkg-sig --sign builder ./output/pika-session*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-session*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-session*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-session*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-session*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
