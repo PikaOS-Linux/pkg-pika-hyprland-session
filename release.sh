55902871c87f0b239fd662444ef21aeb27956bce
ferrreo <harderthanfire@gmail.com>
2023-06-11 22:49:09 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 # Sign the packages
-dpkg-sig --sign builder ./output/pika-session*.deb
+dpkg-sig --sign builder ./output/pika-hyprland-session*.deb
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-session*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-hyprland-session*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-session*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-hyprland-session*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
