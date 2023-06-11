ab2e87ce0ba26fc06da4ecd39f230cc4fb6828f1
ferrreo <harderthanfire@gmail.com>
2023-06-11 22:56:16 +0100

    first

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-cd ./pika-session
+cd ./pika-hyprland-session
 
 # Get build deps
 apt-get build-dep ./ -y
